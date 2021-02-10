a50f28012a367e8a9e9a149adaaaedb5a489e913
= <=>
2021-02-10 12:12:42 +0000

    indentation didn't go through correctly. now fixed

--- dbHandler.py
+++ dbHandler.py
@@ -2,27 +2,27 @@ from replit import db
 
 class DB:
 	def __init__(self,key):
-	self.key = key
+		self.key = key
 
 	@property
 	def inDB(self):
-	try:
-		self.data
-		return True
-	except:
-		return False
+		try:
+			self.data
+			return True
+		except:
+			return False
 
 	@property
 	def delete(self):
-	del db[self.key]
+		del db[self.key]
 
 	@property
 	def data(self):
-	return db[self.key]
+		return db[self.key]
 
 	def equals(self,newData):
-	if self.inDB:
-		self.delete
-	
-	db[self.key] = newData
+		if self.inDB:
+			self.delete
+		# finished deleting so add the new Data
+		db[self.key] = newData
 	

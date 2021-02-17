4e6d2fc91dc1473f8e70494f232986d8874c5123
= <=>
2021-02-17 12:52:21 +0000

    created output and intensity rules

--- commands.py
+++ commands.py
@@ -8,9 +8,27 @@ class Command():
 		self.intensity = intensity
 
 	@property
-	def output(self):
-		return {}
+	def intensity(self) -> int:
+		return self.__intensity
+	
+	@intensity.setter
+	def intensity(self,value:int) -> None:
+		if value >= 0:
+			self.__intensity = value
+		else:
+			raise ValueError('Intensity has to be greater than or equal to 0')
 
+
+	@property
+	def output(self) -> dict:
+		outDict = {'name':self.name}
+		if self.advType in [-1,1]:
+			outDict['advType'] = self.advType
+			# setattr(outDict,'advType',)
+		if self.intensity > 0:
+			# setattr(outDict,'intensity',self.intensity)
+			outDict['intensity'] = self.intensity
+		return outDict
 
 
 
@@ -26,10 +44,30 @@ if __name__ == '__main__':
 			self.assertEqual(c.advType, 0)
 			self.assertEqual(c.intensity, 0)
 
-		def test_output(self):
+		def test_output_noDefaultValues(self):
 			c = Command('TestingName')
 			out = c.output
 			self.assertTrue(isinstance(out,dict))
+			self.assertEqual(out['name'], 'TestingName')
+			self.assertFalse('advType' in out.keys())
+			self.assertFalse('intensity' in out.keys())
+
+		def test_output_nonDefaultValues(self):
+			c = Command('TestingName',1,100)
+			out = c.output
+			self.assertTrue(isinstance(out,dict))
+			self.assertEqual(out['name'], 'TestingName')
+			self.assertEqual(out['advType'], 1)
+			self.assertEqual(out['intensity'], 100)
+
+		def test_negativeIntensity(self):
+			with self.assertRaises(ValueError):
+				_ = Command('TestingName',1,-1)
+
+			c = Command('TestingName',1,100)
+			with self.assertRaises(ValueError):
+				c.intensity = -100
+
 
 
 	unittest.main()
